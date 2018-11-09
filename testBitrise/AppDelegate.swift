0445d9caa8873788182721ce28710786b20d0f69
Xi Xia <xxia@newrelic.com>
2018-11-09 11:51:17 -0800

    local build can crash and dDSYM is uploaded

--- testBitrise/AppDelegate.swift
+++ testBitrise/AppDelegate.swift
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        NewRelic.start(withApplicationToken:"AAd2cc26a1deed0b254aa48e260b4f6d5faea1a2a8")
+        NRLogger.setLogLevels(NRLogLevelALL.rawValue)
+ NewRelic.start(withApplicationToken:"AAd2cc26a1deed0b254aa48e260b4f6d5faea1a2a8")
+        
 
         return true
     }
@@ -30,6 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        NewRelic.crashNow("testBitrise crash")
+
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
